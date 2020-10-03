f96da3d72735e1fea6da982904a80ea6a90f98d2
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-10-03 01:24:28 -0400

    Finished basic axios requests in app.js

--- github-user/src/App.js
+++ github-user/src/App.js
@@ -1,20 +1,67 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-const users = [];
+const followers = [];
+const user = [];
 
 class App extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      users
+      username: "",
+      user: [],
+      followers: []
     };
+  };
+  handleChange = (e) => {
+    console.log(e.target.value)
+    this.setState({
+      ...this.state,
+      username: e.target.value
+    });
+  };
+
+  componentDidMount() {
+    console.log("Component did mount");
+    axios
+      .get(`https://api.github.com/users/Veilios`)
+      .then((res) => {
+        this.setState({
+          ...this.state,
+          user: res.data.message
+        })
+        .catch((err) => console.log("Error 1", err));
+      });
+  };
+
+  componentDidUpdate(PrevState) {
+    if (PrevState !== this.state.user) {
+      console.log("User has changed")
+    }
+  };
+
+  getUser = () => {
+    axios
+      .get(`https://api.github.com/users/${this.state.username}`)
+      .then((res) => {
+        this.setState({
+          ...this.state,
+          username: "",
+          user: res.data.message
+        });
+      })
+      .catch((err) => console.log("Error 2", err))
   };
 
   render() {
     return (
       <div>
+        <h1>
+          Github User
+        </h1>
+        <input type="text" className="user-search" onChange={this.handleChange} value={this.state.username} placeholder="Github Username" />
       </div>
     );
   }
